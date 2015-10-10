7581163ec087a1e9bcb71368a6688032f7dde3f0
Andrew Aitken-Fincham <andrew@silverstripe.co.nz>
2015-10-10 17:00:47 +1300

    add

--- mysite/code/RUGPage.php
+++ mysite/code/RUGPage.php
@@ -2,10 +2,36 @@
 class RUGPage extends Page {
 
 	private static $db = array(
+		'Gender' => 'Varchar(10)',
+		'Name' => 'Varchar(50)',
+		'Street' => 'Varchar(50)',
+		'City' => 'Varchar(50)',
+		'State' => 'Varchar(50)',
+		'Zip' => 'Integer',
+		'Email' => 'Varchar(50)',
+		'Username' => 'Varchar(50)',
+		'Password' => 'Varchar(50)',
+		'Salt' => 'Varchar(50)',
+		'MD5' => 'Varchar(50)',
+		'SHA1' => 'Varchar(50)',
+		'SHA256' => 'Varchar(50)',
+		'Registered' => 'Integer',
+		'DOB' => 'Integer',
+		'Phone' => 'Varchar(15)',
+		'Cell' => 'Varchar(15)',
+		'DNI' => 'Varchar(15)',
+		'PicL' => 'Image',
+		'PicM' => 'Image',
+		'PicThumb' => 'Image',
+		'Version' => 'Float',
+		'Nationality' => 'Varchar(2)',
+		'Seed' => 'Varchar(15)'
 	);
 
 	private static $has_one = array(
 	);
+
+	private static $can_be_root = false;
 
 }
 class RUGPage_Controller extends Page_Controller {
