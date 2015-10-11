613d46f484ce0d374edbaf0861f6fafa68c55989
Andrew Aitken-Fincham <andrew@silverstripe.co.nz>
2015-10-11 16:10:38 +1300

    log more json to console

--- themes/snugbugrug/javascript/GetRUGUser.js
+++ themes/snugbugrug/javascript/GetRUGUser.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   $("#RUGButton").click(function(){
     $.ajax({
-      url: 'http://api.randomuser.me/?format=sql&nat=au&lego',
+      url: 'http://api.randomuser.me/?lego',
       dataType: 'json',
       success: function(data){
-      console.log(data);
+        if(!data['error']){
+          console.log(data);
+          var user = data.results[0].user;
+          console.log(user);
+          $('li[data-label="name"]').attr('data-value', user.name.first+' '+user.name.last);
+          $('li[data-label="email"]').attr('data-value', user.email);
+        }
       }
     });
   });
