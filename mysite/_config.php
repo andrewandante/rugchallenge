9af9bcb35422b1f5284b02bbe06fefefa0a6f845
Andrew Aitken-Fincham <andrew@silverstripe.co.nz>
2015-10-18 19:20:54 +1300

    something odd has happened

--- mysite/_config.php
+++ mysite/_config.php
@@ -10,3 +10,4 @@ require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
+ 
--- mysite/code/RUGPageHolder.php
+++ mysite/code/RUGPageHolder.php
@@ -22,60 +22,60 @@ class RUGPageHolder_Controller extends Page_Controller {
     'getuser'
   );
 
-  public function RUGUserForm() {
-    $fields = FieldList::create(
-      TextField::create('FirstName', 'First Name'),
-      TextField::create('Surname'),
-      TextField::create('Email')
-    );
-    $actions = FieldList::create(FormAction::create('doRUGUser', 'Generate a Random User!'));
-    return Form::create($this, 'RUGUserForm', $fields, $actions);
+  // public function RUGUserForm() {
+  //   $fields = FieldList::create(
+  //     TextField::create('FirstName', 'First Name'),
+  //     TextField::create('Surname'),
+  //     TextField::create('Email')
+  //   );
+  //   $actions = FieldList::create(FormAction::create('doRUGUser', 'Generate a Random User!'));
+  //   return Form::create($this, 'RUGUserForm', $fields, $actions);
+	//
+  // }
 
-  }
-
-  public function doRUGUser($data, $form) {
-    $member = Member::create();
-    $form->saveInto($member);
-    $member->write();
-    return $this->redirectBack();
-  }
+  // public function doRUGUser($data, $form) {
+  //   $member = Member::create();
+  //   $form->saveInto($member);
+  //   $member->write();
+  //   return $this->redirectBack();
+  // }
 
 	public function getuser(){
-
-
-	  echo "the function is running\n";
 	  $response = NULL;
 	  if (isset($_GET)) {
-		print_r($_GET);
-		$var = Convert::raw2xml(array_keys($_GET)[1]);
-		echo "var\n";
-		var_dump($var);
-		if ($var == 'rug') {
-			$getfield = "?nat=au";
-		} elseif ($var == 'lego') {
-			$getfield = "?lego";
-			$isLego = true;
-		} else {
-			$getfield = false;
+			$var = Convert::raw2xml(array_keys($_GET)[1]);
+			if ($var == 'rug') {
+				$getfield = "?nat=au";
+				$isLego = false;
+			} elseif ($var == 'lego') {
+				$getfield = "?lego";
+				$isLego = true;
+			} else {
+				$getfield = false;
+			}
+			if ($getfield != false) {
+				$request = "http://api.randomuser.me/" . $getfield;
+				$curl = curl_init($request);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				$response = json_decode(curl_exec($curl), true);
+			}
+			if ($response != NULL) {
+				$newmember = Member::create();
+				$fields = array(
+					'Firstname' => $response['results']['0']['user']['name']['first'],
+					'Surname' => $response['results']['0']['user']['name']['last'],
+					'Email' => $response['results']['0']['user']['email'],
+					'Username' => $response['results']['0']['user']['username'],
+					'Lego' => $isLego
+				);
+				// var_dump($fields);
+				var_dump($newmember);
+				$fields->addTo($newmember);
+				$newmember->write();
+
+			}
 		}
-		echo "getfield\n";
-		var_dump($getfield);
-		if ($getfield != false) {
-			$request = "http://api.randomuser.me/" . $getfield;
-			echo "request\n";
-			var_dump($request);
-			$curl = curl_init($request);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = json_decode(curl_exec($curl), true);
-			echo "response\n";
-			var_dump($response);
-		}
-	  	echo " ";
-		echo "\r\n";
-		echo "first name";
-		var_dump($response['results']['0']['user']['name']['first']);
-	  	
-	  }
-	  return $response;
+
+	  return $newmember;
 	}
 }
